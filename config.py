bc50a1f609d5b8768cd64939de518fe7c52a958e
n-h-byte <hritwiksupercell@gmail.com>
2025-11-02 09:46:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -39,7 +39,7 @@ API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 OWNER_ID = int(os.getenv("OWNER_ID", None))
-OWNER_USERNAME = os.getenv("OWNER_USERNAME", "BillieXMusic")
+OWNER_USERNAME = os.getenv("OWNER_USERNAME", "SlayWithRose")
 BOT_USERNAME = os.getenv("BOT_USERNAME", "BillieMusicBot")
 
 # ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
